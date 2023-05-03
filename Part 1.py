eeeec420ae1ad2b8135bab831f3ec41ad2032ca6
Charles Amoah <amoahc0705@students.bowiestate.edu>
2023-05-03 18:12:20 -0400

    test changes

---
+++ Part 1.py
@@ -0,0 +1,10 @@
+def Maryland_detector(Maryland_temp) :
+    if Maryland_temp < 10:
+        return "Maryland is Extremely Cold"
+    elif Maryland_temp >= 10 and Maryland_temp <= 30 :
+        return "Maryland is Cold"
+    elif Maryland_temp >=31 and Maryland_temp <= 45 :
+        return "Maryland is Good"
+    elif Maryland_temp >= 46 and Maryland_temp <= 70 :
+        return "Maryland is Warm"
+    elif Maryland_temp >=
