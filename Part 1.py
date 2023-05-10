ef5c3766b50250458c53e812053ad08816d819a7
PreciousAA <adeyinkap0731@students.bowiestate.edu>
2023-05-10 17:54:36 -0400

    Project

--- Part 1.py
+++ Part 1.py
@@ -12,3 +12,24 @@ def temperature_detector(state_name, temperature):
         return state_name + "is hot"
     else:
         return state_name + "is extremely hot"
+
+
+#<10 - Extemely cold
+#10 - 30 - cold
+#31 - 45 - good
+#46 - 70 - Warm
+#71 - 85 - Hot
+#>86 - Extremly hot
+def temperature_reading():
+    Maryland_temp = int(input("Enter Maryland temperature"))
+    Texas_temp = int(input("Enter Texas temperature"))
+    Maine_temp = int(input("Enter Maine temperature"))
+
+    print(temperature_detector("Maryland, Maryland_temp"))
+    print(temperature_detector("Texas, Texas_temp"))
+    print(temperature_detector("Maine, Maine_temp"))
+    print(favourable(Maryland_temp,Texas_temp,Maine_temp))
+
+
+
+temperature_reading()
