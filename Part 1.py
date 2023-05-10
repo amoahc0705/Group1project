923e37761307b87f7a81e70bd80990683ea55940
PreciousAA <adeyinkap0731@students.bowiestate.edu>
2023-05-10 17:42:41 -0400

    project

--- Part 1.py
+++ Part 1.py
@@ -1,64 +1,22 @@
+#<10 - Extemely cold
+#10 - 30 - cold
+#31 - 45 - good
+#46 - 70 - Warm
+#71 - 85 - Hot
+#>86 - Extremly hot
 def temperature_reading():
-    Maryland_temp = int(input("Enter Maryland temperature: "))
-    Texas_temp = int(input("Enter Texas temperature: "))
-    Maine_temp = int(input("Enter Maine temperature: "))
-    # <10 - Extremely cold
-    # 10 - 30 - cold
-    # 31 - 45 - good
-    # 46 - 70 - warm
-    # 71 - 85 - Hot
-    # >86 - Extremely hot
-    print(Maryland_detector(Maryland_temp))
-    print(Texas_detector(Texas_temp))
-    print(Maine_detector(Maine_temp))
-    print(favourable(Maryland_temp, Texas_temp, Maine_temp))
-def Texas_detector(Texas_temp):
-    if Texas_temp <10:
-        return "Texas is Extremely cold"
-    elif Texas_temp >=10 and Texas_temp <=30:
-        return "Texas is cold"
-    elif Texas_temp >=31 and Texas_temp <=45:
-        return "Texas is good"
-    elif Texas_temp >=46 and Texas_temp <=70:
-        return "Texas is warm"
-    elif Texas_temp >=71 and Texas_temp <=85:
-        return "Texas is hot"
-    else:
-        return "Texas is Extremely Hot"
+    Maryland_temp = int(input("Enter Maryland temperature"))
+    Texas_temp = int(input("Enter Texas temperature"))
+    Maine_temp = int(input("Enter Maine temperature"))
 
-def Maryland_detector(Maryland_temp) :
-    if Maryland_temp < 10:
-        return "Maryland is Extremely Cold"
-    elif Maryland_temp >= 10 and Maryland_temp <= 30 :
-        return "Maryland is Cold"
-    elif Maryland_temp >=31 and Maryland_temp <= 45 :
-        return "Maryland is Good"
-    elif Maryland_temp >= 46 and Maryland_temp <= 70 :
-        return "Maryland is Warm"
-    elif Maryland_temp >=
+    print(temperature_dector("Maryland, Maryland_temp"))
+    print(temperature_detector("Texas, Texas_temp"))
+    print(temperature_detector("Maine, Maine_temp"))
+    print(favourable(Maryland_temp,Texas_temp,Maine_temp))
 
 
-def Maine_detector(Maine_temp):
-    if Maine_temp <10:
-        return "Maine is Extremely cold"
-    elif Maine_temp >=10 and Maine_temp <=30:
-        return "Maine is cold"
-    elif Maine_temp >=31 and Maine_temp <=45:
-        return "Maine is good"
-    elif Maine_temp >=46 and Maine_temp <= 70:
-        return "Maine is warm"
-    elif Maine_temp >=71 and Maine_temp <= 85:
-        return "Maine is hot"
-    else:
-        return "Maine is extremely  Hot"
-    def favourable(Maryland_temp, Texas_temp, Maine_temp):
-        if Maryland_temp >=50 and Maryland_temp <=70 and Texas_temp >=50 and Texas_temp <=70 and Maine_temp>=50 and Maine_temp<=70:
-            return min(Maryland_temp, Texas_temp, Maine_temp), "is most favourable"
-        elif Maryland_temp>=50 and Maryland_temp <=70:
-            return "Maryland is  most favourable"
-        elif Texas_temp >=50 and Texas_temp <=70:
-            return "Texas is most favourable"
-        elif Maine_temp >=50 and Maine_temp <= 70:
-            return "Maine is most favourable"
-        else:
-            return "Dial 911 for Help!"
+
+
+
+
+temperature_reading()
