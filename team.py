c4a83b55d6492b5a0a465bd8dbce52687f3187b1
Dammy337 <ainao0401@students.bowiestate.edu>
2023-05-24 19:04:49 -0400

    project

--- team.py
+++ team.py
@@ -11,3 +11,19 @@ def temperature_detector(state_name, temperature):
         return state_name + "is hot"
     else:
         return state_name +"is Extremely Hot"
+def favourable(Maryland_temp, Texas_temp, Maine_temp):
+    if Maryland_temp >= 50 and Maryland_temp <=70 and Texas_temp >=50 and Texas_temp <= 70 and Maine_temp >=50 and Maine_temp <=70:
+        if Maryland_temp < Texas_temp and Maryland_temp < Maine_temp:
+            return "Maryland is most favourable"
+        elif Texas_temp < Maryland_temp and Texas_temp < Maine_temp:
+            return "Texas is most favourable"
+        else:
+            return "Maine is most favourable"
+    elif Maryland_temp >=50 and Maryland_temp <=70:
+        return "Maryland is most favourable"
+    elif Texas_temp >=50 and Texas_temp <=70:
+        return "Texas is most favourable"
+    elif Maine_temp >=50 and Maine_temp <=70:
+        return "Maine is most favourable"
+    else:
+        return "Dial 911 for help!"
