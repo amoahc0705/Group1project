17179d0427132a4fe9fd79f2c9996174082a1c8a
Charles Amoah <amoahc0705@students.bowiestate.edu>
2023-05-24 18:11:03 -0400

    Project

--- team.py
+++ team.py
@@ -1,3 +1,17 @@
+def temperature_detector(state_name, temperature):
+    if temperature < 10:
+        return state_name + “is extremely cold”
+    elif temperature >=10 and temperature <= 30:
+        return state_name + “is cold”
+    elif temperature >=31 and temperature <=45:
+        return state_name + “is good”
+    elif temperature >=46 and temperature <=70:
+        return state_name + “is warm”
+    elif temperature >=71 and temperature <=85:
+        return state_name + “is hot”
+    else:
+        return state_name + “is extremely hot”
+
 
 def favourable(Maryland_temp, Texas_temp, Maine_temp):
     if Maryland_temp >= 50 and Maryland_temp <=70 and Texas_temp >=50 and Texas_temp <= 70 and Maine_temp >=50 and Maine_temp <=70:
